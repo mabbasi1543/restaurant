76033a2b8b73f021c460522f68b060240229ced1
mabbasi1543 <m.abbasi1543@gmail.com>
2022-09-09 13:42:33 +0430

    completed home page made box class usable on other pages

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,7 @@
 import createElement from "./utility"
 
 const renderContact = (parent) => {
-    const box = createElement(parent, "div", "", "contactBox")
+    const box = createElement(parent, "div", "", "box")
     createElement(box, "p", "contact info:");
     createElement(box, "br");
     createElement(box, "p", "+1 123 123 1234");
--- src/home.js
+++ src/home.js
@@ -1,7 +1,12 @@
 import createElement from "./utility"
 
 const renderHome = (parent) => {
-    createElement(parent, "div", "home")
+    const box = createElement(parent, "div", "", "box")
+    const img = createElement(box, "img")
+    img.src = "https://www.recipetineats.com/wp-content/uploads/2017/11/Chicken-Doner-Kebab-2.jpg?resize=650,975";
+    createElement(box, "br");
+    createElement(box, "h3", "Welcome to kebabland")
+    createElement(box, "h5", "Hope you enjoy our food")
 }
 
 export default renderHome;
